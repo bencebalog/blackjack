0d2d0850bf75563f2670d8be9e6554b10ee3edfe
bencebalog <balogbence991@gmail.com>
2024-04-17 13:10:14 +0200

    Valami

--- src/main/java/com/example/bj/HelloController.java
+++ src/main/java/com/example/bj/HelloController.java
@@ -2,13 +2,25 @@ package com.example.bj;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @FXML private TextField txF;
+    @FXML private HBox d;
+    @FXML private HBox j;
+    
+
+    @FXML private void onHitClick(){
+
+    }
+
+    @FXML private void onStandClick(){
+
+    }
+
+    @FXML private void onJoinClick(){
+
     }
 }
